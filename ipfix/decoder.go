15134174472b71f0ea0ecd27ad8f15dfad5e279c
mehrdad arshad rad <mehrdad@edgecast.com>
2017-01-30 22:06:50 +0000

    init ipfix decoder w/ hmsg decoder

---
+++ ipfix/decoder.go
@@ -0,0 +1,67 @@
+package ipfix
+
+import (
+	"encoding/binary"
+	"io"
+)
+
+type IFDecoder struct {
+	reader io.Reader
+}
+
+type MessageHeader struct {
+	Version    uint16
+	Length     uint16
+	ExportTime uint32
+	SequenceNo uint32
+	DomainID   uint32
+}
+
+func NewDecoder(r io.Reader) IFDecoder {
+	return IFDecoder{
+		reader: r,
+	}
+}
+
+func (d *IFDecoder) Decode() error {
+	var (
+		h   MessageHeader
+		err error
+	)
+
+	if err = h.unmarshal(d.reader); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (h *MessageHeader) unmarshal(d io.Reader) error {
+	var err error
+
+	if err = read(d, &h.Version); err != nil {
+		return err
+	}
+
+	if err = read(d, &h.Length); err != nil {
+		return err
+	}
+
+	if err = read(d, &h.ExportTime); err != nil {
+		return err
+	}
+
+	if err = read(d, &h.SequenceNo); err != nil {
+		return err
+	}
+
+	if err = read(d, &h.DomainID); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func read(r io.Reader, v interface{}) error {
+	return binary.Read(r, binary.BigEndian, v)
+}
