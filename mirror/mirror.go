e75b7b391581aef6eb080be89716e2ae7fa2e322
mehrdad arshad rad <mehrdad@edgecast.com>
2017-02-01 18:14:32 +0000

    raw socket methods

--- mirror/mirror.go
+++ mirror/mirror.go
@@ -5,30 +5,56 @@ import (
 	"syscall"
 )
 
+const (
+	IPv4HLen = 20
+	IPv6HLen = 40
+	UDPHLen  = 8
+)
+
 type Conn struct {
 	family int
 	sotype int
 	proto  int
 	fd     int
-	raddr  *net.UDPAddr
+	raddr  syscall.Sockaddr
 }
 
-func DialUDP(raddr *net.UDPAddr) (Conn, error) {
+func NewRawConn(raddr net.IP) (Conn, error) {
 	var err error
-
 	conn := Conn{
-		sotype: syscall.SOCK_DGRAM,
-		proto:  syscall.IPPROTO_UDP,
-		raddr:  raddr,
+		sotype: syscall.SOCK_RAW,
+		proto:  syscall.IPPROTO_RAW,
 	}
 
-	if raddr.IP.To4 != nil {
+	if ipv4 := raddr.To4(); ipv4 != nil {
+		ip := [4]byte{}
+		copy(ip[:], ipv4)
+
 		conn.family = syscall.AF_INET
-	} else {
+		conn.raddr = &syscall.SockaddrInet4{
+			Port: 0,
+			Addr: ip,
+		}
+	} else if ipv6 := raddr.To16(); ipv6 != nil {
+		ip := [16]byte{}
+		copy(ip[:], ipv6)
+
 		conn.family = syscall.AF_INET6
+		conn.raddr = &syscall.SockaddrInet6{
+			Addr: ip,
+		}
+
 	}
 
 	conn.fd, err = syscall.Socket(conn.family, conn.sotype, conn.proto)
 
 	return conn, err
+}
+
+func (c *Conn) Send(b []byte) error {
+	return syscall.Sendto(c.fd, b, 0, c.raddr)
+}
+
+func (c *Conn) Close(b []byte) error {
+	return syscall.Close(c.fd)
 }
