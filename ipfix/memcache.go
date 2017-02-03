b82a2b08343a8c89df73d1a9a55035cfd15a7e03
mehrdad arshad rad <mehrdad@edgecast.com>
2017-02-03 19:31:38 +0000

    init ipfix memory cache - shard support

---
+++ ipfix/memcache.go
@@ -0,0 +1,68 @@
+package ipfix
+
+import (
+	"encoding/binary"
+	"hash/fnv"
+	"net"
+	"sync"
+	"time"
+)
+
+var ShardNo = 32
+
+type MemCache []TemplatesShard
+
+type Data struct {
+	TemplateRecord
+	timestamp int64
+}
+
+type TemplatesShard struct {
+	template map[string]Data
+	sync.RWMutex
+}
+
+func newCache() MemCache {
+	m := make(MemCache, ShardNo)
+	for i := 0; i < ShardNo; i++ {
+		m[i] = TemplatesShard{template: make(map[string]Data)}
+	}
+	return m
+}
+
+func (m MemCache) getShard(id uint16, addr net.IP) (TemplatesShard, []byte) {
+	b := make([]byte, 2)
+	binary.BigEndian.PutUint16(b, id)
+	key := append(addr, b...)
+
+	hash := fnv.New32()
+	hash.Write(key)
+	return m[uint(hash.Sum32())%uint(ShardNo)], key
+}
+
+func (m *MemCache) insert(id uint16, addr net.IP, tr TemplateRecord) {
+	shard, key := m.getShard(id, addr)
+	shard.Lock()
+	defer shard.Lock()
+	shard.template[string(key)] = Data{tr, time.Now().Unix()}
+}
+
+func (m *MemCache) retrieve(id uint16, addr net.IP) (TemplateRecord, bool) {
+	shard, key := m.getShard(id, addr)
+	shard.RLock()
+	defer shard.RLock()
+	v, ok := shard.template[string(key)]
+	return v.TemplateRecord, ok
+}
+
+func (m *MemCache) remove(id int, addr string) {
+	// TODO
+}
+
+func (m *MemCache) cleanup(id int, addr string) {
+	// TODO
+}
+
+func (m *MemCache) dump(id int, addr string) {
+	// TODO
+}
