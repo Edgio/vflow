89c34d9dfd98442872490138d8348429fd9f6f05
mehrdad arshad rad <mehrdad@edgecast.com>
2017-02-07 07:13:46 +0000

    add more test

--- mirror/mirror_test.go
+++ mirror/mirror_test.go
@@ -24,6 +24,8 @@ package mirror
 import (
 	"net"
 	"testing"
+
+	"golang.org/x/net/ipv4"
 )
 
 func TestNewRawConn(t *testing.T) {
@@ -34,5 +36,29 @@ func TestNewRawConn(t *testing.T) {
 	}
 	if c.family != 2 {
 		t.Error("expected family# 2, got", c.family)
+	}
+}
+
+func TestIPv4Header(t *testing.T) {
+	ipv4RawHeader := NewIPv4HeaderTpl(17)
+	b := ipv4RawHeader.Marshal()
+	h, err := ipv4.ParseHeader(b)
+	if err != nil {
+		t.Error("unexpected error", err)
+	}
+	if h.Version != 4 {
+		t.Error("expect version: 4, got", h.Version)
+	}
+	if h.Protocol != 17 {
+		t.Error("expect protocol: 17, got", h.Protocol)
+	}
+	if h.TTL != 64 {
+		t.Error("expect TTL: 64, got", h.TTL)
+	}
+	if h.Len != 20 {
+		t.Error("expect Len: 20, got", h.Len)
+	}
+	if h.Checksum != 0 {
+		t.Error("expect Checksum: 0, got", h.Checksum)
 	}
 }
