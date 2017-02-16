3fc83848b6835acc07ab98404d388e7f31b29108
mehrdad arshad rad <mehrdad@edgecast.com>
2017-02-16 19:04:33 +0000

    rename buff name

--- ipfix/marshal.go
+++ ipfix/marshal.go
@@ -33,118 +33,118 @@ import (
 var errUknownMarshalDataType = errors.New("unknown data type to marshal")
 
 // JSONMarshal encodes IPFIX message
-func (m *Message) JSONMarshal(e *bytes.Buffer) ([]byte, error) {
-	e.WriteString("{")
+func (m *Message) JSONMarshal(b *bytes.Buffer) ([]byte, error) {
+	b.WriteString("{")
 
 	// encode agent id
-	m.encodeAgent(e)
+	m.encodeAgent(b)
 
 	// encode header
-	m.encodeHeader(e)
+	m.encodeHeader(b)
 
 	// encode data sets
-	if err := m.encodeDataSet(e); err != nil {
+	if err := m.encodeDataSet(b); err != nil {
 		return nil, err
 	}
 
-	e.WriteString("}")
+	b.WriteString("}")
 
-	return e.Bytes(), nil
+	return b.Bytes(), nil
 }
 
-func (m *Message) encodeDataSet(e *bytes.Buffer) error {
+func (m *Message) encodeDataSet(b *bytes.Buffer) error {
 	var length, dsLength int
 
-	e.WriteString("\"DataSets\":")
+	b.WriteString("\"DataSets\":")
 	dsLength = len(m.DataSets)
 
-	e.WriteString("[")
+	b.WriteString("[")
 	for i := range m.DataSets {
 		length = len(m.DataSets[i])
 
-		e.WriteString("[")
+		b.WriteString("[")
 		for j := range m.DataSets[i] {
-			e.WriteString("{\"ID\":")
-			e.WriteString(strconv.FormatInt(int64(m.DataSets[i][j].ID), 10))
-			e.WriteString(",\"Value\":")
+			b.WriteString("{\"ID\":")
+			b.WriteString(strconv.FormatInt(int64(m.DataSets[i][j].ID), 10))
+			b.WriteString(",\"Value\":")
 
 			switch m.DataSets[i][j].Value.(type) {
 			case uint:
-				e.WriteString(strconv.FormatInt(int64(m.DataSets[i][j].Value.(uint)), 10))
+				b.WriteString(strconv.FormatInt(int64(m.DataSets[i][j].Value.(uint)), 10))
 			case uint8:
-				e.WriteString(strconv.FormatInt(int64(m.DataSets[i][j].Value.(uint8)), 10))
+				b.WriteString(strconv.FormatInt(int64(m.DataSets[i][j].Value.(uint8)), 10))
 			case uint16:
-				e.WriteString(strconv.FormatInt(int64(m.DataSets[i][j].Value.(uint16)), 10))
+				b.WriteString(strconv.FormatInt(int64(m.DataSets[i][j].Value.(uint16)), 10))
 			case uint32:
-				e.WriteString(strconv.FormatInt(int64(m.DataSets[i][j].Value.(uint32)), 10))
+				b.WriteString(strconv.FormatInt(int64(m.DataSets[i][j].Value.(uint32)), 10))
 			case uint64:
-				e.WriteString(strconv.FormatInt(int64(m.DataSets[i][j].Value.(uint64)), 10))
+				b.WriteString(strconv.FormatInt(int64(m.DataSets[i][j].Value.(uint64)), 10))
 			case int:
-				e.WriteString(strconv.FormatInt(int64(m.DataSets[i][j].Value.(int)), 10))
+				b.WriteString(strconv.FormatInt(int64(m.DataSets[i][j].Value.(int)), 10))
 			case int8:
-				e.WriteString(strconv.FormatInt(int64(m.DataSets[i][j].Value.(int8)), 10))
+				b.WriteString(strconv.FormatInt(int64(m.DataSets[i][j].Value.(int8)), 10))
 			case int16:
-				e.WriteString(strconv.FormatInt(int64(m.DataSets[i][j].Value.(int16)), 10))
+				b.WriteString(strconv.FormatInt(int64(m.DataSets[i][j].Value.(int16)), 10))
 			case int32:
-				e.WriteString(strconv.FormatInt(int64(m.DataSets[i][j].Value.(int32)), 10))
+				b.WriteString(strconv.FormatInt(int64(m.DataSets[i][j].Value.(int32)), 10))
 			case int64:
-				e.WriteString(strconv.FormatInt(int64(m.DataSets[i][j].Value.(int64)), 10))
+				b.WriteString(strconv.FormatInt(int64(m.DataSets[i][j].Value.(int64)), 10))
 			case float32:
-				e.WriteString(strconv.FormatFloat(float64(m.DataSets[i][j].Value.(float32)), 'E', -1, 32))
+				b.WriteString(strconv.FormatFloat(float64(m.DataSets[i][j].Value.(float32)), 'E', -1, 32))
 			case float64:
-				e.WriteString(strconv.FormatFloat(m.DataSets[i][j].Value.(float64), 'E', -1, 64))
+				b.WriteString(strconv.FormatFloat(m.DataSets[i][j].Value.(float64), 'E', -1, 64))
 			case string:
-				e.WriteString("\"")
-				e.WriteString(m.DataSets[i][j].Value.(string))
-				e.WriteString("\"")
+				b.WriteString("\"")
+				b.WriteString(m.DataSets[i][j].Value.(string))
+				b.WriteString("\"")
 			case net.IP:
-				e.WriteString("\"")
-				e.WriteString(m.DataSets[i][j].Value.(net.IP).String())
-				e.WriteString("\"")
+				b.WriteString("\"")
+				b.WriteString(m.DataSets[i][j].Value.(net.IP).String())
+				b.WriteString("\"")
 			case net.HardwareAddr:
-				e.WriteString("\"")
-				e.WriteString(m.DataSets[i][j].Value.(net.HardwareAddr).String())
-				e.WriteString("\"")
+				b.WriteString("\"")
+				b.WriteString(m.DataSets[i][j].Value.(net.HardwareAddr).String())
+				b.WriteString("\"")
 			case []uint8:
-				e.WriteString("\"")
-				e.WriteString(fmt.Sprintf("0x%x", m.DataSets[i][j].Value.([]uint8)))
-				e.WriteString("\"")
+				b.WriteString("\"")
+				b.WriteString(fmt.Sprintf("0x%x", m.DataSets[i][j].Value.([]uint8)))
+				b.WriteString("\"")
 			default:
 				return errUknownMarshalDataType
 			}
 			if j < length-1 {
-				e.WriteString("},")
+				b.WriteString("},")
 			} else {
-				e.WriteString("}")
+				b.WriteString("}")
 			}
 		}
 		if i < dsLength-1 {
-			e.WriteString("],")
+			b.WriteString("],")
 		} else {
-			e.WriteString("]")
+			b.WriteString("]")
 		}
 	}
-	e.WriteString("]")
+	b.WriteString("]")
 
 	return nil
 }
 
-func (m *Message) encodeHeader(e *bytes.Buffer) {
-	e.WriteString("\"Header\":{\"Version\":")
-	e.WriteString(strconv.FormatInt(int64(m.Header.Version), 10))
-	e.WriteString(",\"Length\":")
-	e.WriteString(strconv.FormatInt(int64(m.Header.Length), 10))
-	e.WriteString(",\"ExportTime\":")
-	e.WriteString(strconv.FormatInt(int64(m.Header.ExportTime), 10))
-	e.WriteString(",\"SequenceNo\":")
-	e.WriteString(strconv.FormatInt(int64(m.Header.SequenceNo), 10))
-	e.WriteString(",\"DomainID\":")
-	e.WriteString(strconv.FormatInt(int64(m.Header.DomainID), 10))
-	e.WriteString("},")
+func (m *Message) encodeHeader(b *bytes.Buffer) {
+	b.WriteString("\"Header\":{\"Version\":")
+	b.WriteString(strconv.FormatInt(int64(m.Header.Version), 10))
+	b.WriteString(",\"Length\":")
+	b.WriteString(strconv.FormatInt(int64(m.Header.Length), 10))
+	b.WriteString(",\"ExportTime\":")
+	b.WriteString(strconv.FormatInt(int64(m.Header.ExportTime), 10))
+	b.WriteString(",\"SequenceNo\":")
+	b.WriteString(strconv.FormatInt(int64(m.Header.SequenceNo), 10))
+	b.WriteString(",\"DomainID\":")
+	b.WriteString(strconv.FormatInt(int64(m.Header.DomainID), 10))
+	b.WriteString("},")
 }
 
-func (m *Message) encodeAgent(e *bytes.Buffer) {
-	e.WriteString("\"AgentID\":\"")
-	e.WriteString(m.AgentID)
-	e.WriteString("\",")
+func (m *Message) encodeAgent(b *bytes.Buffer) {
+	b.WriteString("\"AgentID\":\"")
+	b.WriteString(m.AgentID)
+	b.WriteString("\",")
 }
