8962ef84f2fb4af4cf2f61a83ec9c3f776221169
mehrdad arshad rad <arshad.rad@gmail.com>
2017-04-24 21:13:02 +0000

    netflow v9 run method w/o cache and rpc

--- vflow/netflow_v9.go
+++ vflow/netflow_v9.go
@@ -23,6 +23,12 @@ package main
 
 import (
 	"net"
+	"strconv"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/VerizonDigital/vflow/producer"
 )
 
 // NetflowV9 represents netflow v9 collector
@@ -51,6 +57,18 @@ type NetflowV9Stats struct {
 	Workers      int32
 }
 
+var (
+	netflowV9UDPCh = make(chan NetflowV9UDPMsg, 1000)
+	netflowV9MQCh  = make(chan []byte, 1000)
+
+	// ipfix udp payload pool
+	netflowV9Buffer = &sync.Pool{
+		New: func() interface{} {
+			return make([]byte, opts.NetflowV9UDPSize)
+		},
+	}
+)
+
 // NewNetflowV9 constructs NetflowV9
 func NewNetflowV9() *NetflowV9 {
 	return &NetflowV9{
@@ -62,13 +80,72 @@ func NewNetflowV9() *NetflowV9 {
 
 func (i *NetflowV9) run() {
 	//TODO
+	// exit if the ipfix is disabled
+	if !opts.NetflowV9Enabled {
+		logger.Println("netflowv9 has been disabled")
+		return
+	}
+
+	hostPort := net.JoinHostPort(i.addr, strconv.Itoa(i.port))
+	udpAddr, _ := net.ResolveUDPAddr("udp", hostPort)
+
+	conn, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	atomic.AddInt32(&i.stats.Workers, int32(i.workers))
+	for n := 0; n < i.workers; n++ {
+		go func() {
+			wQuit := make(chan struct{})
+			i.pool <- wQuit
+			i.netflowV9Worker(wQuit)
+		}()
+	}
+
+	logger.Printf("netflow v9 is running (workers#: %d)", i.workers)
+
+	go func() {
+		p := producer.NewProducer(opts.MQName)
+
+		p.MQConfigFile = opts.MQConfigFile
+		p.MQErrorCount = &i.stats.MQErrorCount
+		p.Logger = logger
+		p.Chan = netflowV9MQCh
+		p.Topic = opts.NetflowV9Topic
+
+		if err := p.Run(); err != nil {
+			logger.Fatal(err)
+		}
+	}()
+
+	go func() {
+		if !opts.DynWorkers {
+			logger.Println("netflow v9 dynamic worker disabled")
+			return
+		}
+
+		i.dynWorkers()
+	}()
+
+	for !i.stop {
+		b := netflowV9Buffer.Get().([]byte)
+		conn.SetReadDeadline(time.Now().Add(1e9))
+		n, raddr, err := conn.ReadFromUDP(b)
+		if err != nil {
+			continue
+		}
+		atomic.AddUint64(&i.stats.UDPCount, 1)
+		netflowV9UDPCh <- NetflowV9UDPMsg{raddr, b[:n]}
+	}
+
 }
 
 func (i *NetflowV9) shutdown() {
 	//TODO
 }
 
-func (i *NetflowV9) netflowWorker(wQuit chan struct{}) {
+func (i *NetflowV9) netflowV9Worker(wQuit chan struct{}) {
 	// TODO
 }
 
